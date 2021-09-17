4ee127fe18b147895b43a42c925ab03e528227ac
ilua <misyurailya5@gmail.com>
2021-09-17 23:28:10 +0300

    New Nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //Global
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 //Local
@@ -10,15 +10,14 @@ import Home from './pages/Home';
 import Train from './pages/Train';
 
 import {
-  Button,
-  Container,
-  Dropdown,
+  Checkbox,
+  Grid,
   Header,
   Icon,
+  Image,
   Menu,
   Segment,
   Sidebar,
-  Image,
 } from 'semantic-ui-react'
 import Diray from './pages/Diray';
 
@@ -27,63 +26,6 @@ import Diray from './pages/Diray';
 
 
 
-const VerticalSidebar = ({ animation, direction, visible }) => (
-  <Sidebar
-    as={Menu}
-    animation={animation}
-    direction={direction}
-    icon='labeled'
-    inverted
-    vertical
-    visible={visible}
-    width='thin'
-  >
-    <Menu.Item style={{display:'flex', justifyContent:'center'}} >
-      <div style={{display:'flex', justifyContent:'center'}}>
-      <Image size='mini' src='/logo.png'  />
-      </div>
-      </Menu.Item>
-    <Menu.Item as='a' onClick={(e) => {
-              e.preventDefault();
-              window.location.href='http://localhost:3000/';
-              }}>
-      <Icon name='home'  />
-      Home
-    </Menu.Item>
-    <Menu.Item as='a' onClick={(e) => {
-        e.preventDefault();
-        window.location.href='http://localhost:3000/program';
-        }}>
-      <Icon name='gamepad' />
-      Program
-    </Menu.Item>
-    <Menu.Item as='a' onClick={(e) => {
-        e.preventDefault();
-        window.location.href='http://localhost:3000/diray';
-        }}>
-      <Icon name='camera' />
-      Diray
-    </Menu.Item>
-    <Menu.Item as='a'>
-      <Icon name='food' />
-      Food
-    </Menu.Item>
-  </Sidebar>
-)
-
-function exampleReducer(state, action) {
-  switch (action.type) {
-    case 'CHANGE_ANIMATION':
-      return { ...state, animation: action.animation, visible: !state.visible }
-    case 'CHANGE_DIMMED':
-      return { ...state, dimmed: action.dimmed }
-    case 'CHANGE_DIRECTION':
-      return { ...state, direction: action.direction, visible: false }
-    default:
-      throw new Error()
-  }
-}
-
 
 
 
@@ -91,17 +33,7 @@ function exampleReducer(state, action) {
 function App() {
 
 
-
-
-  const [state, dispatch] = React.useReducer(exampleReducer, {
-    animation: 'overlay',
-    direction: 'left',
-    dimmed: false,
-    visible: false,
-  })
-
-  const { animation, dimmed, direction, visible } = state
-  const vertical = direction === 'bottom'
+  const [visible, setVisible] = React.useState(false)
 
 
 
@@ -111,52 +43,53 @@ function App() {
 
 
 
-    <Sidebar.Pushable as={Segment} style={{ overflow: 'hidden' }}>
 
-      {!vertical && (
-        <VerticalSidebar
-          animation={animation}
-          direction={direction}
+
+    <Grid columns={1}>
+
+
+    <Grid.Column>
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar
+          as={Menu}
+          animation='push'
+          icon='labeled'
+          inverted
+          onHide={() => setVisible(false)}
+          vertical
           visible={visible}
-        />
-      )}
+          width='thin'
+        >
+          <Menu.Item as='a'>
+            <Icon name='home' />
+            Home
+          </Menu.Item>
+          <Menu.Item as='a'>
+            <Icon name='gamepad' />
+            Games
+          </Menu.Item>
+          <Menu.Item as='a'>
+            <Icon name='camera' />
+            Channels
+          </Menu.Item>
+        </Sidebar>
 
-      <Sidebar.Pusher dimmed={dimmed && visible}>
-
-      <Menu inverted attached='top' borderless>
-          <Menu.Item icon="align justify"           onClick={() =>
-            dispatch({ type: 'CHANGE_ANIMATION', animation: 'push' })
-          }></Menu.Item>
-        <Container>
-        <Menu.Item as='a' header>
-          <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
-        </Menu.Item>
-        <Menu.Item as='a'>Home</Menu.Item>
-
-        <Dropdown item simple text='Dropdown'>
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className='dropdown icon' />
-              <span className='text'>Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-      </Menu>
-      </Sidebar.Pusher>
-
+        <Sidebar.Pusher>
 
 
     
+          <Menu>
+
+
+          <Menu.Item icon="align justify"
+           onClick={(e, data) =>  console.log("clicked") }
+          />
+                <Checkbox
+        onClick={(e, data) => setVisible(data.checked)}
+      />
+
+      </Menu>
+
       <BrowserRouter>
         <Switch>
 
@@ -191,13 +124,21 @@ function App() {
           <Redirect to='/error' />
 
         </Switch>
+        
       </BrowserRouter>
 
 
-    </Sidebar.Pushable>
+
+      </Sidebar.Pusher>
+      </Sidebar.Pushable>
+      </Grid.Column>
+      </Grid>
+
+
 
 
   );
+  
 }
 
 export default App;
