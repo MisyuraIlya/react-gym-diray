81fb9da846bbbf14bf97317a1b0214b48fb12a8f
ilua <misyurailya5@gmail.com>
2021-09-20 19:07:40 +0300

    added array for program list

--- src/components/program/ProgramList.jsx
+++ src/components/program/ProgramList.jsx
@@ -1,16 +1,43 @@
 //Global
 import React from 'react';
-import {Segment,Container,Header,Icon,Image,Button,Modal} from 'semantic-ui-react'
+import {
+  Segment,
+  Container,
+  Header,
+  Icon,
+  Image,
+  Button,
+  Modal,
+  Grid
+} from 'semantic-ui-react'
 
 //Local
 import SplitProgram from './SplitProgram';
 import CreateProgram from './CreateProgram';
 import MyPagination from '../MyPagination';
 
+const ProgramState = [
+  {
+    programName: 'program A',
+    programStyle: 'Power',
+    programDescription: 'aaaaaaaa',
+    img: '/img/1.jpg'
+  }, {
+    programName: 'program B',
+    programStyle: 'Mass',
+    programDescription: 'bbbbbbbb',
+    img: '/img/2.jpg'
+  }, {
+    programName: 'program C',
+    programStyle: 'Fitness',
+    programDescription: 'bbbbbbbb',
+    img: '/img/2.jpg'
+  }
+]
+
 const ProgramList = () => {
   return (
     <Container>
-
       <Header
         as='h1'
         style={{
@@ -26,33 +53,8 @@ const ProgramList = () => {
           }}>
             <CreateProgram/>
           </div>
-          <Segment>
-            <Segment basic>
-              <div>
-                <Modal
-                  trigger={< Button positive > Edit </Button>}
-                  header='Program Editor'
-                  content={< SplitProgram />}
-                  actions={[
-                  'Close', {
-                    key: 'done',
-                    content: 'Done',
-                    positive: true
-                  }
-                ]}/>
-                <Header as='h2' icon textAlign='center'>
-                  <Icon name='users' circular/>
-                  <Header.Content>Power Lifting</Header.Content>
-                </Header>
-                <Image
-                  centered
-                  size='large'
-                  src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png'/>
-              </div>
-            </Segment>
-          </Segment>
 
-          <Segment>
+          {ProgramState.map(list => <Segment>
             <Segment basic>
               <div>
                 <Modal
@@ -66,43 +68,26 @@ const ProgramList = () => {
                     positive: true
                   }
                 ]}/>
-                <Header as='h2' icon textAlign='center'>
-                  <Icon name='users' circular/>
-                  <Header.Content>Power Lifting</Header.Content>
-                </Header>
-                <Image
-                  centered
-                  size='large'
-                  src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png'/>
-              </div>
-            </Segment>
-          </Segment>
+                <Grid columns={3} >
+                  <Grid.Row>
+                    <Grid.Column>
+                    </Grid.Column>
+                    <Grid.Column>
+                    <Image src={list.img} size='medium' circular/>
 
-          <Segment>
-            <Segment basic>
-              <div>
-                <Modal
-                  trigger={< Button positive > Edit </Button>}
-                  header='Program Editor'
-                  content={< SplitProgram />}
-                  actions={[
-                  'Close', {
-                    key: 'done',
-                    content: 'Done',
-                    positive: true
-                  }
-                ]}/>
+                    </Grid.Column>
+                    <Grid.Column>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
                 <Header as='h2' icon textAlign='center'>
-                  <Icon name='users' circular/>
-                  <Header.Content>Power Lifting</Header.Content>
+                  <Header.Content>{list.programName}</Header.Content>
                 </Header>
-                <Image
-                  centered
-                  size='large'
-                  src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png'/>
+                <Header as='h3' textAlign='center'>{list.programDescription}</Header>
               </div>
             </Segment>
-          </Segment>
+          </Segment>)}
+
         </div>
       </Container>
       <MyPagination/>
